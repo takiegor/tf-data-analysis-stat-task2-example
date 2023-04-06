8a5aadaa9362f9cd329811bdd8e86db4d9edbf74
takiegor <95751108+takiegor@users.noreply.github.com>
2023-04-07 02:32:54 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,12 +4,14 @@ import numpy as np
 from scipy.stats import norm
 
 
-chat_id = 451121038 # Ваш chat ID, не меняйте название переменной
+chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    alpha = 1 - p
-    ci = np.percentile(x, [1-alpha, alpha])
-    return ci
+    n = len(x)
+    t = np.abs (np.random.standard_t(df = n-1, size = 100000))
+    std_error = np.std (x, ddof = 1) / np.sqrt(n)
+    margin_of_error = t[np.searchsorted(np.sort(t), 1-(1-p)/2)] * std_error
+    return (np.mean(x) - margin_of_error, np.mean(x) + margin_of_error)
