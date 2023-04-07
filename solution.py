57efa683f2281d5c8c7746b9b10cccb9ba3c3961
takiegor <95751108+takiegor@users.noreply.github.com>
2023-04-07 22:38:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -13,6 +13,6 @@ def solution(p: float, x: np.array) -> tuple:
     loc = x.mean()
     b = 2 * loc - 0.083
     
-    scale = np.sqrt(((b - a)**2) / 12) / np.sqrt(len(x))
+    scale = np.sqrt(((b - 0.083)**2) / 12) / np.sqrt(len(x))
     return b - scale * uniform.ppf(1 - alpha / 2), \
            b - scale * uniform.ppf(alpha / 2)
