df911b639386f6678a901a03d792442a424d23a2
takiegor <95751108+takiegor@users.noreply.github.com>
2023-04-07 22:18:04 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import uniform
 
 
 chat_id = 451121038 # Ваш chat ID, не меняйте название переменной
@@ -10,8 +10,9 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    n = len(x)
-    t = np.abs (np.random.standard_t(df = n-1, size = 100000))
-    std_error = np.std (x, ddof = 1) / np.sqrt(n)
-    margin_of_error = t[np.searchsorted(np.sort(t), 1-(1-p)/2)] * std_error
-    return (np.mean(x) - margin_of_error, np.mean(x) + margin_of_error)
+    loc = x.mean()
+    b = 2 * loc - 0.083
+    
+    scale = np.sqrt(((b - a)**2) / 12) / np.sqrt(len(x))
+    return b - scale * uniform.ppf(1 - alpha / 2), \
+           b - scale * uniform.ppf(alpha / 2)
