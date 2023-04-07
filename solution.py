3309abcee3594ef4203162c7a3a874bd5863016b
takiegor <95751108+takiegor@users.noreply.github.com>
2023-04-07 23:47:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -14,5 +14,5 @@ def solution(p: float, x: np.array) -> tuple:
     b = 2 * loc - 0.083
     alpha = 1 - p
     scale = np.sqrt(((b - 0.083)**2) / 12) / np.sqrt(len(x))
-    return b - 2 * scale * uniform.ppf(1 - alpha / 2), \
+    return b + 2 * scale * uniform.ppf(1 - alpha / 2), \
            b + 2 * scale * uniform.ppf(alpha / 2)
